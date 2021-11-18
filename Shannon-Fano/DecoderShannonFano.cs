e9608a9316bf35fa47a5d9dfc5c2e695fa66af22
aktivgo <AktivGo01062001@gmail.com>
2021-11-18 10:32:29 +0300

    Add comments

--- Shannon-Fano/DecoderShannonFano.cs
+++ Shannon-Fano/DecoderShannonFano.cs
@@ -4,16 +4,26 @@ namespace Shannon_Fano
 {
     public static class DecoderShannonFano
     {
+        /// <summary>
+        /// Декодирует входную строку с помощью словаря
+        /// </summary>
+        /// <param name="inputStr"></param>
+        /// <param name="table"></param>
+        /// <returns></returns>
         public static string Decode(string inputStr, Dictionary<string, char> table)
         {
             string decode = "";
 
             string letterCode = "";
+            // Проходим по каждому числу закодированной строке
             for (int i = 0; i < inputStr.Length; i++)
             {
+                // Прибавляем число к коду
                 letterCode += inputStr[i];
+                // Если словарь содержит данный код
                 if (table.ContainsKey(letterCode))
                 {
+                    // Добавляем символ с таким кодом в результат
                     decode += table[letterCode];
                     letterCode = "";
                 }
--- Shannon-Fano/EncoderShannonFano.cs
+++ Shannon-Fano/EncoderShannonFano.cs
@@ -8,15 +8,23 @@ namespace Shannon_Fano
 {
     public static class EncoderShannonFano
     {
-        private static Dictionary<char, string> _resultTable;
-        private static List<string> _codes;
-        private static double _codingPrice;
+        private static Dictionary<char, string> _resultTable; // Словарь символ-двоичная строка
+        private static List<string> _codes; // Двоичные коды символов
+        private static double _codingPrice; // Цена кодирования
 
+        /// <summary>
+        /// Возвращает таблицу с закодированными символами
+        /// </summary>
+        /// <returns></returns>
         public static Dictionary<char, string> GetResultTable()
         {
             return _resultTable;
         }
 
+        /// <summary>
+        /// Инициализирует лист двоичных кодов
+        /// </summary>
+        /// <param name="str"></param>
         private static void InitCodes(string str)
         {
             _codes = new List<string>();
@@ -26,11 +34,19 @@ namespace Shannon_Fano
             }
         }
 
+        /// <summary>
+        /// Возвращает цену кодирования
+        /// </summary>
+        /// <returns></returns>
         public static double GetCodingPrice()
         {
             return _codingPrice;
         }
 
+        /// <summary>
+        /// Считает цену кодирования
+        /// </summary>
+        /// <param name="probabilities">Словарь символ-вероятность</param>
         private static void CalculateCodingPrice(Dictionary<char, double> probabilities)
         {
             _codingPrice = 0;
@@ -40,6 +56,11 @@ namespace Shannon_Fano
             }
         }
 
+        /// <summary>
+        /// Считает вероятности символов
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
         private static Dictionary<char, double> GetCharsProbability(string str)
         {
             Dictionary<char, double> result = new Dictionary<char, double>();
@@ -56,6 +77,7 @@ namespace Shannon_Fano
                     {
                         result['&']++;
                     }
+
                     continue;
                 }
 
@@ -77,49 +99,72 @@ namespace Shannon_Fano
             return result;
         }
 
+        /// <summary>
+        /// Рекурсивно создает результативную таблицу
+        /// </summary>
+        /// <param name="p">Лист вероятностей символов</param>
+        /// <param name="start">Индекс начала</param>
+        /// <param name="end">Индекс конца</param>
         private static void RecursiveCreateTable(List<double> p, int start, int end)
         {
+            // Если осталась 1 строка, то конец рекурсии
             if (Math.Abs(start - end) <= 1)
             {
                 return;
             }
 
+            // Считаем медиану
             int index = GetMedian(p, start, end);
 
+            // Слева от медианы добавляем 1
             for (int i = start; i < index; i++)
             {
                 _codes[i] += "1";
             }
 
+            // Справа 0
             for (int i = index; i < end; i++)
             {
                 _codes[i] += "0";
             }
 
+            // Вызываем рекурсию от левого отрезка медианы и от правого
             RecursiveCreateTable(p, start, index);
             RecursiveCreateTable(p, index, end);
         }
 
+        /// <summary>
+        /// Считает медиану вероятностей, начиная с индекса start до индекса end
+        /// </summary>
+        /// <param name="map">Лист вероятностей символов</param>
+        /// <param name="start">Индекс начала</param>
+        /// <param name="end">Индекс конца</param>
+        /// <returns></returns>
         private static int GetMedian(List<double> map, int start, int end)
         {
             double min = Int32.MaxValue, sumLeft = 0, sumRight = 0;
             int res = 0;
 
+            // Проходим по каждому индексу
             for (int i = 1; i < end - start; i++)
             {
+                // Берем вероятности слева и считаем сумму
                 for (int j = start; j < start + i; j++)
                 {
                     sumLeft += map[j];
                 }
 
+                // Берем вероятности справа и считаем сумму
                 for (int j = start + i; j < end; j++)
                 {
                     sumRight += map[j];
                 }
 
+                // Ищем минимум разницы между этими суммами
                 if (min > Math.Abs(sumLeft - sumRight))
                 {
                     min = Math.Abs(sumLeft - sumRight);
+                    // Запоминаем индекс, при котором разность минимальна
                     res = start + i;
                 }
 
@@ -130,6 +175,11 @@ namespace Shannon_Fano
             return res;
         }
 
+        /// <summary>
+        /// Записывает результат в файл
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="encode"></param>
         private static void WriteResultToFile(string str, string encode)
         {
             string path = "decode/";
@@ -153,17 +203,26 @@ namespace Shannon_Fano
             Console.WriteLine("Результат сохранен в файл " + fileName);
         }
 
+        /// <summary>
+        /// Кодирует входную строку
+        /// </summary>
+        /// <param name="inputStr"></param>
+        /// <returns></returns>
         public static string Encode(string inputStr)
         {
+            // Инициализируем таблицу с результатом
             _resultTable = new Dictionary<char, string>();
             InitCodes(inputStr);
 
+            // Считаем словарь с вероятностями и сортируем по убыванию вероятностей
             Dictionary<char, double> probabilities = GetCharsProbability(inputStr);
             probabilities = probabilities.OrderByDescending(pair => pair.Value)
                 .ToDictionary(pair => pair.Key, pair => pair.Value);
 
+            // Рекурсивно создаем результативную таблицу
             RecursiveCreateTable(probabilities.Values.ToList(), 0, probabilities.Count);
 
+            // Записываем коды символов в результат
             for (int i = 0; i < probabilities.Count; i++)
             {
                 char character = probabilities.Keys.ToArray()[i];
@@ -180,8 +239,10 @@ namespace Shannon_Fano
                 }
             }
 
+            // Считаем цену кодирования
             CalculateCodingPrice(probabilities);
 
+            // С помощью получившегося словаря кодов кодирует входное сообщение
             string encode = "";
             foreach (var character in inputStr)
             {
@@ -194,6 +255,7 @@ namespace Shannon_Fano
                 encode += _codes[probabilities.Keys.ToList().IndexOf(character)];
             }
 
+            // Записываем результат в файл
             WriteResultToFile(inputStr, encode);
             return encode;
         }
